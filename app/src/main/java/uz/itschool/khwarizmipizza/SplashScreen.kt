9cf51e8a19aa3f4e34ce6c11d4ca382d1aae5300
PC679 <akzumrad@gmail.com>
2023-03-02 15:42:39 +0500

    hiii

---
+++ app/src/main/java/uz/itschool/khwarizmipizza/SplashScreen.kt
@@ -0,0 +1,21 @@
+package uz.itschool.khwarizmipizza
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        Handler().postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }, 1500
+        )
+    }
+}
