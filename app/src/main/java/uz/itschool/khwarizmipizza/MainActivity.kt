c74a6621a136d7a00b0eec6b39e9200e8c31da65
PC670 <ismoilovashahina@gmail.com>
2023-03-09 13:25:38 +0500

    s

--- app/src/main/java/uz/itschool/khwarizmipizza/MainActivity.kt
+++ app/src/main/java/uz/itschool/khwarizmipizza/MainActivity.kt
@@ -1,15 +1,37 @@
 package uz.itschool.khwarizmipizza
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import uz.itschool.khwarizmipizza.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnEnglish.setOnClickListener {
+            setAppLocale(this, "en")
+            val intent = Intent(this, Registration::class.java)
+            startActivity(intent)
+        }
+        binding.btnUzbek.setOnClickListener{
+            setAppLocale(this, "uz")
+            val intent = Intent(this, Registration::class.java)
+            startActivity(intent)
+        }
 
-
+    }
+    fun setAppLocale(context: Context, language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val config = context.resources.configuration
+        config.setLocale(locale)
+        context.createConfigurationContext(config)
+        context.resources.updateConfiguration(config, context.resources.displayMetrics)
     }
 }
---
+++ app/src/main/java/uz/itschool/khwarizmipizza/Registration.kt
@@ -0,0 +1,11 @@
+package uz.itschool.khwarizmipizza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
